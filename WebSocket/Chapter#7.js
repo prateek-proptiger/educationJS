24f7b963d87591423dfb9de86f7ffc01d48aff73
prateek-proptiger <prateeksehgal.iitbhu@gmail.com>
2016-03-28 12:33:40 +0530

    Chapter#7

---
+++ WebSocket/Chapter#7.js
@@ -0,0 +1,14 @@
+Chapter-7 (Debugging and Tools)
+
+We have learned how to integrate a new technology 'WebSocket' in our project but how to debug when things dont work as
+expected ?
+A typical WebSocket life cycle is:
+1) Opening Handshake.
+2) Sending and Receiving Frames.
+3) Closing Handshake.
+
+The Handshake
+-------------
+The data received by server from a valid client must include headers like Host, Connection, Upgrade etc.
+
+Not of my level at present.
