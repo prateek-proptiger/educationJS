a8c34cd97c06aa14a0f3ed10c1c79fa6aa877f76
prateek-proptiger <prateeksehgal.iitbhu@gmail.com>
2016-04-08 18:31:31 +0530

    w3Schools CSS

---
+++ CSS/Fonts.js
@@ -0,0 +1,23 @@
+Fonts
+=====
+To study.
+
+Links
+=====
+To study.
+
+Lists
+=====
+In HTML, lists are basically of two types ul or ol.
+'list-style-type': The type of icon to be used for list based on list type. E.g. 'list-style-type': 'circle'.
+'list-style-image': Specify the url for icon as url('/image/xyz.png');
+'list-style-position': Specifies whether the list-item markers should appear inside or outside the content flow.
+
+Box Model
+=========
+All elements in HTML can be considered as boxes.
+Where each element is made up of Margin + Border + Padding + Content.
+Thus total height/width of each element should be calculated considering margin, border and padding as well.
+
+Outline
+=======
