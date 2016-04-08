198f2be3a18847db8c8e4710e8d16dab99368538
prateek-proptiger <prateeksehgal.iitbhu@gmail.com>
2016-04-08 17:36:49 +0530

    w3Schools CSS

---
+++ CSS/Backgrounds.js
@@ -0,0 +1,57 @@
+Backgrounds
+===========
+CSS background properties:
+- background-color: Sets background color of any element.
+- background-image: Sets background image of any element.
+- background-repeat:
+    Sets whether the image should repeat horizontally or vertically.
+    It has values: repeat-x, repeat-y, no-repeat.
+- background-attachment: To set the background image to be fixed at position, set this property to fixed.
+- background-position: Sets the position of background image.
+
+
+Borders
+=======
+- border-style: dotted
+- border-width: 5px
+- border-color: red
+
+All above can be divided as top, right, bottom and left in the same order.
+
+Margins
+=======
+Used to generate white space around an element. They cannot have background color.
+CSS has properties to specify margin for each side: margin-top, margin-left, margin-bottom and margin-right.
+Each property can have a value:
+- auto: The browser calculates the margin. Basically centers the element in its container horizontally.
+- length: In px, cm, pt etc.
+- %: In percent.
+- inherit: Margin to be inherited from parent element.
+
+Padding
+=======
+Used to set white space between element text and border.
+Similar to margin it has individual properties for each side.
+Each property can have a value:
+- length: In px, cm, pt etc.
+- %: In percent.
+- inherit: Margin to be inherited from parent element.
+
+Height and Weight
+=================
+To set height and width of any element.
+max-height and max-width are used to set the max permissible values.
+
+Text
+====
+'color': Used to set the color of text.
+'text-align': Used to set the alignment of text. It can be left, right, center or justify. Justify specifies each row to be
+streched so that they have equal width.
+'text-decoration': Used to set decorations on text. E.g. overline, underline and line-through.
+'text-transform': Used to convert text to uppercase, lowercase or capitalize(First letter).
+'text-indent': To specify the identation of first line of text. E.g text-indent: '50px';
+'letter-spacing': Used to set spacing between characters. E.g text-indent: '3px';
+'line-height': Used to set space between lines. E.g. line-height: 1.8;
+'direction': Use to set the direction of text. E.g. direction: 'rtl' for right to left.
+'word-spacing': Used to set spacing between words.
+Other important properties are: 'text-shadow', 'white-space', 'vertical-align'
